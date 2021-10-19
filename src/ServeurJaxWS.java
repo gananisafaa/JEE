918e976f41b1abe8b432d1d484eb1e6d9b58b14f
Safâa Ganani <ganani.safaa.works@gmail.com>
2021-10-19 11:49:21 +0100

    lgtm

--- src/ServeurJaxWS.java
+++ src/ServeurJaxWS.java
@@ -6,7 +6,7 @@ public class ServeurJaxWS {
     public static void main(String[] args) {
         String url = "http://0.0.0.0:8686/";
         Endpoint.publish(url, new BanqueService());
-        System.out.println("Web service déployé sur" + url);
+        System.out.println("Web service déployé sur " + url);
     }
 }
 
