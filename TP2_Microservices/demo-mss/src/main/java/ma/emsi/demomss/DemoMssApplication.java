7f29f35f870c75213bb1c722c573b0d723de118b
Safâa Ganani <ganani.safaa.works@gmail.com>
2021-10-27 19:32:39 +0100

    add TP microservice

---
+++ TP2_Microservices/demo-mss/src/main/java/ma/emsi/demomss/DemoMssApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.demomss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMssApplication.class, args);
+    }
+
+}
